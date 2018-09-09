adb418fdfa63298bf55e9c14eb491896e85e2b61
Sergei Sergeev <sergeev.srg@gmail.com>
2018-09-09 11:10:03 +0300

    Updated to match SPFx 1.6. New methods were added.

--- SPFxAadClient/Constants.cs
+++ SPFxAadClient/Constants.cs
@@ -12,7 +12,7 @@ namespace SPFxAadClient
         public const string GraphServiceObjectId = "00000002-0000-0000-c000-000000000000";
         public const string TenantId = "948fd9cc-9adc-40d8-851e-acefa17ab66c"; // <--- change to your TenantId
         public const string ClientId = "dd1155f9-3f40-4190-b5e4-3988a6c18250"; // <--- change to your ClientId
-        public const string SpoCeApplicationId = "c58637bb-e2e1-4312-8a00-04b5ffcd3403"; 
+        public const string SpoCeApplicationId = "d02ebf4e-0497-4b18-817d-06ad82d41540"; // <-- SharePoint Online Client Extensibility Web Application Principal Id (might be different in your tenant)
         public const string MsGrpaphApplicationId = "00000003-0000-0000-c000-000000000000";
     }
 }
--- SPFxAadClient/Program.cs
+++ SPFxAadClient/Program.cs
@@ -11,7 +11,7 @@ namespace SPFxAadClient
             var client = AuthenticationHelper.GetActiveDirectoryClientAsUser();
 
             Console.WriteLine("Run operations for signed-in user");
-            Console.WriteLine("[a] - add\\update permission \n[d] - deletes permission\nPlease enter your choice:");
+            Console.WriteLine("[a] - add\\update permission Graph permissions \n[d] - deletes Graph permissions \n[c] - deletes api-sso permissions \n[d] - adds api-sso permissions \n[e] - prints all grants added to SharePoint CE web app \nPlease enter your choice:");
 
             ConsoleKeyInfo key = Console.ReadKey();
             switch (key.KeyChar)
@@ -20,9 +20,20 @@ namespace SPFxAadClient
                     AddSpfxMSGraphPermissions(client, "Calendars.Read,Mail.ReadWrite").Wait();
                     Console.WriteLine("Successfully added permissions for MS Graph!");
                     break;
-                case 'd':
+                case 'b':
                     RemoveSpfxMSGraphPermissions(client).Wait();
                     Console.WriteLine("Successfully deleted permissions for MS Graph!");
+                    break;
+                case 'c':
+                    RemoveThirdPartAPIPermissions(client, "6fc2655e-04cd-437d-a50d-0c1a31383775").Wait();
+                    Console.WriteLine("Successfully deleted permissions for api-sso!");
+                    break;
+                case 'd':
+                    AddThirdPartAPIPermissions(client, "6fc2655e-04cd-437d-a50d-0c1a31383775", "user_impersonation").Wait();
+                    Console.WriteLine("Successfully added permissions for api-sso!");
+                    break;
+                case 'e':
+                    PrintGrants(client).Wait();
                     break;
             }
         }
@@ -89,6 +100,96 @@ namespace SPFxAadClient
             if (existingGrant != null)
             {
                 await existingGrant.DeleteAsync();
+            }
+        }
+
+        public static async Task RemoveThirdPartAPIPermissions(ActiveDirectoryClient client, string thirdPartyClientId)
+        {
+            var spoCeServicePrinicipal = await client.ServicePrincipals
+                .Where(sp => sp.AppId == GlobalConstants.SpoCeApplicationId).ExecuteSingleAsync();
+
+            var thirdPartyServicePrincipal = await client.ServicePrincipals
+                .Where(sp => sp.AppId == thirdPartyClientId).ExecuteSingleAsync();
+
+            var grants = await client.Oauth2PermissionGrants.ExecuteAsync();
+            OAuth2PermissionGrant existingGrant = null;
+            foreach (IOAuth2PermissionGrant grant in grants.CurrentPage)
+            {
+                if (grant.ClientId == spoCeServicePrinicipal.ObjectId &&
+                    grant.ResourceId == thirdPartyServicePrincipal.ObjectId)
+                {
+                    existingGrant = (OAuth2PermissionGrant)grant;
+                }
+            }
+
+            if (existingGrant != null)
+            {
+                await existingGrant.DeleteAsync();
+            }
+        }
+
+        public static async Task AddThirdPartAPIPermissions(ActiveDirectoryClient client, string thirdPartyClientId, string scope)
+        {
+            var spoCeServicePrinicipal = await client.ServicePrincipals
+                .Where(sp => sp.AppId == GlobalConstants.SpoCeApplicationId).ExecuteSingleAsync();
+
+            var thirdPartyServicePrincipal = await client.ServicePrincipals
+                .Where(sp => sp.AppId == thirdPartyClientId).ExecuteSingleAsync();
+
+            var grants = await client.Oauth2PermissionGrants.ExecuteAsync();
+            OAuth2PermissionGrant existingGrant = null;
+            foreach (IOAuth2PermissionGrant grant in grants.CurrentPage)
+            {
+                if (grant.ClientId == spoCeServicePrinicipal.ObjectId &&
+                    grant.ResourceId == thirdPartyServicePrincipal.ObjectId)
+                {
+                    existingGrant = (OAuth2PermissionGrant)grant;
+                }
+            }
+
+            if (existingGrant != null)
+            {
+                existingGrant.Scope = scope;
+                await existingGrant.UpdateAsync();
+            }
+            else
+            {
+                var auth2PermissionGrant = new OAuth2PermissionGrant
+                {
+                    ClientId = spoCeServicePrinicipal.ObjectId,
+                    ConsentType = "AllPrincipals",
+                    PrincipalId = null,
+                    ExpiryTime = DateTime.Now.AddYears(10),
+                    ResourceId = thirdPartyServicePrincipal.ObjectId,
+                    Scope = scope
+                };
+
+                await client.Oauth2PermissionGrants.AddOAuth2PermissionGrantAsync(auth2PermissionGrant);
+            }
+        }
+
+        public static async Task PrintGrants(ActiveDirectoryClient client)
+        {
+            var spoCeServicePrinicipal = await client.ServicePrincipals
+                .Where(sp => sp.AppId == GlobalConstants.SpoCeApplicationId).ExecuteSingleAsync();
+
+            var grants = await client.Oauth2PermissionGrants.ExecuteAsync();
+            foreach (IOAuth2PermissionGrant grant in grants.CurrentPage)
+            {
+                if (grant.ClientId == spoCeServicePrinicipal.ObjectId)
+                {
+                    var objectId = grant.ResourceId;
+                    var servicePrincipal = await client.ServicePrincipals.Where(sp => sp.ObjectId == objectId).ExecuteSingleAsync();
+
+                    Console.WriteLine("*********************");
+                    Console.WriteLine("");
+                    Console.WriteLine($"AppDisplayName: {servicePrincipal.AppDisplayName}");
+                    Console.WriteLine($"ConsentType: {grant.ConsentType}");
+                    Console.WriteLine($"Scope: {grant.Scope}");
+                    Console.WriteLine($"ClientId: {grant.ClientId}");
+                    Console.WriteLine($"ResourceId: {grant.ResourceId}");
+                    Console.WriteLine("");
+                }
             }
         }
     }
